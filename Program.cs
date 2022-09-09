fb5080261857333f168fe5696d53afee3e38c21f
BohdanBoss <bogdanburduluk@gmail.com>
2022-09-09 13:17:58 +0300

    Added info files

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿
+string[] ChangeArrray(string[] array)
+{
+    string[] str = new string[array.Length];
+    for(int i = 0; i < str.Length; i++)
+    {
+        if(array[i].Length <= 3)
+          str[i] = array[i];  
+    }
+    return str;
+}
+
+void ShowArray(string [] array)
+{
+    for(int i = 0; i < array.Length; i++)
+    Console.Write(array[i] + " ");
+    Console.WriteLine();
+}
+
+string[] array = {"Hello", "2", "world", ":-)"};
+string[] str = ChangeArrray(array);
+ShowArray(array);
+ShowArray(str);
